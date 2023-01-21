0612aea0e71555fe68d13945e9a0c628f4c8025b
Ari Bengiyat <ari@aribengiyat.com>
2023-01-21 14:39:26 -0500

    Fixed changes after code review

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,13 @@ import { io } from 'socket.io-client'
 
 const socket = io();
 
-socket.on('banana', (message) => {
-  console.log('From server banana: ', message);
-  document.querySelector('.chat').innerHTML = message;
-})
+// socket.on('banana', (message) => {
+//   console.log('From server banana: ', message);
+//   document.querySelector('.chat').innerHTML = message;
+// })
 
 
-const sendMessage = () => {
-  socket.emit('frontendMessage', '{username: test}')
-}
+// const sendMessage = () => {
+//   socket.emit('frontendMessage', '{username: test}')
+// }
 
-window.sendMessage = sendMessage;
