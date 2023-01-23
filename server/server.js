36cdb4b73855c8b98d619dd3e783543c96629931
Garrett YYY <garrettyan6@gmail.com>
2023-01-22 22:54:55 -0500

    frontend and server websockets setup. action "newEvent" will send name, tiem and event description to server and get back updated list of all events. action "loadEvents" will get current list of events from database when page first load. Plan to test it tomorrow with team.

--- server/server.js
+++ server/server.js
@@ -9,16 +9,22 @@ const io = new Server(http, {});
 
 io.on('connection', (socket) => {
   //We know websosckets are connected
-  console.log('Connected');
-
-  socket.on('disconnect', () => {
-    console.log('user dcd');
-  });
+  console.log('Server connected' + socket.id);
 
   //We will receive event lists form database
-  socket.on('firstLoad', (message) => {
+  socket.on('loadEvents', (message) => {
     console.log('from PostMan:', message);
     io.emit('createEvent', 'response form DS');
+  });
+
+  //listen to action 'newEvent', once receive event from client, store it in databasa
+  socket.on('newEvent', (e) => {
+    //see what event is passed in
+    console.log(e);
+    //create new event in database
+    database.set(e);
+    //send back updated events from database to client with action 'loadEvents'
+    io.emit('loadEvent', 'response from DS');
   });
 });
 
