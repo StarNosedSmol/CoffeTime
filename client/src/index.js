d4f1066c319daf166a722826aee7474c56d03666
Ari Bengiyat <ari@aribengiyat.com>
2023-01-23 10:58:54 -0500

    removed frontend changes from be team

--- client/src/index.js
+++ client/src/index.js
@@ -13,25 +13,7 @@ root.render(
     </div>
   </>
 );
-// Once connect, show Client conected and load all events from database
-socket.on('connect', () => {
-  console.log('Client connected');
-  //send action 'loadEvents' to server
-  socket.emit('loadEvents', (message) => {
-    displayMessage(message);
-  });
-  //load events from server by websocket action 'loadEvents'
-  socket.on('loadEvents', (events) => {
-    document.querySelector('.chat').innerHTML = message;
-  });
-});
 
-//Once the eventlistener is ready, use this to send data through websocket to server side.
-example.addEventListener('click', () => {
-  const valueOfInputBox = document.querySelector('input');
-  //send action 'newEvent' to server with name, time and event description
-  socket.emit('newEvent', valueOfInputBox);
-});
 
 // const sendMessage = () => {
 //   socket.emit('frontendMessage', '{username: test}')
