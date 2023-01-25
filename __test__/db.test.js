532d571b50ee5db8af4065ac15397796179ea013
George Vazquez <georgevazquez@Marceas-MacBook-Pro.local>
2023-01-25 14:46:53 -0500

    put it into a test collection for testing purporses

--- __test__/db.test.js
+++ __test__/db.test.js
@@ -12,20 +12,24 @@ describe('insert', () => {
     db = await connection.db();
   });
 
+  beforeEach(async () => {
+    await db.collection('test').deleteMany({});
+  });
+
   it('should insert a basic doc into collection', async () => {
-    const events = db.collection('events');
+    const events = db.collection('test');
   
     const mockEvent = {
       host: "george",
       created: "2023-01-24T21:26:29.050Z",
       eventTime: "12:00",
       details: { 
-          title: "beers",
+          title: "beets",
           }
     };
     await events.insertOne(mockEvent);
     
-    const insertedEvent = await events.findOne({details: { title: 'beers' }});
+    const insertedEvent = await events.findOne({details: { title: 'beets' }});
     console.log(insertedEvent)
     expect(insertedEvent).toEqual(mockEvent);
   });
